67d9cc2a2a0fab48f2ff17b54118d14dd8ec33ef
KG LEE <acrkdduf@naver.com>
2022-02-01 19:11:44 +0900

    feat: 작성한 컴포넌트를 통한 구조 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
-import type { NextPage } from 'next'
-import styles from '../styles/basic.module.css'
+import type { NextPage } from 'next';
+import StyledComponents from '../components/Styled-Components';
+import LinariaComponent from '../components/Linaira/LinariaComponent';
+import StitchesComponent from '../components/Stitches/StitchesComponent';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className={styles.title}>CSS-In-JS</h1>
-    </div>
-  )
-}
+      <h1 style={{ fontSize: '30px', textAlign: 'center' }}>5st Generation: CSS-In-JS</h1>
+      <div style={{ marginTop: '15px', textAlign: 'center' }}>
+        5세대 (이상) Css-In-Js 방식의 라이브러리 중 <br />
+        runtime / zero-runtime / near zero-runtime 방식의 라이브러리를 <br />
+        각각 간단하게 사용해보고 비교 분석해봅시다. <br />
+        (Styled Components는 4세대지만 Css-In-Js의 대표격으로 합류)
+      </div>
 
-export default Home
+      <div style={{ padding: '15px' }}>
+        <StyledComponents />
+        <LinariaComponent />
+        <StitchesComponent />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
