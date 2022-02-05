ef2747c5c91dcb83d42ec47603f96ed15191d362
KG LEE <acrkdduf@naver.com>
2022-02-05 22:38:51 +0900

    fix: stitches 사용방법 오타 수정

--- components/Stitches/Usage/Usage.tsx
+++ components/Stitches/Usage/Usage.tsx
@@ -5,7 +5,7 @@ const Usage = () => {
   const { open, handleClickOnCollapsibleHeader } = useCollapsible();
   return (
     <Stitches.Collapsible>
-      <Stitches.CollapsibleHeader onClick={handleClickOnCollapsibleHeader}>설치방법</Stitches.CollapsibleHeader>
+      <Stitches.CollapsibleHeader onClick={handleClickOnCollapsibleHeader}>사용방법</Stitches.CollapsibleHeader>
       <Stitches.CollapsibleContent open={open}>내용...</Stitches.CollapsibleContent>
     </Stitches.Collapsible>
   );
